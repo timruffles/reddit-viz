4d35b7773725c118fe050ad8dd40ae7ab7f8a326
Tim Ruffles <timruffles@googlemail.com>
2013-08-30 14:41:19 +0100

    relate circle size to story score

--- my_viz.js
+++ my_viz.js
@@ -1,18 +1,39 @@
 function main() {
-  d3.jsonp("http://reddit.com/r/javascript.json?jsonp=d3.jsonp.foo",_.compose(visualize,formatData));
+  d3.jsonp("http://reddit.com/r/javascript.json?jsonp=d3.jsonp.foo",
+      _.compose(visualize,formatSingleSubreddit));
 }
 
 function visualize(redditActivitySources) {
-  d3.select("body")
-    .selectAll("p")
-    .data(redditActivitySources)
+  var sources = d3.select("#viz")
+    .selectAll(".source")
+    .data(redditActivitySources);
+
+  var sourcesEntering = sources
     .enter()
-      .append("p")
-      .text(function(d) { return d });
+      .append("g")
+      .classed("source",true)
+      .attr("transform",function(d,i) {
+        return "translate(250," + i * 250 + ")"
+      });
+
+  sourcesEntering
+    .append("circle")
+    .attr("r",function(d,i) {
+      return d.score * 5;
+    });
+
+  // sources is now UPDATE & ENTER
+  
+  sourcesEntering
+    .append("text")
+    .attr("dx",0)
+    .attr("dy",0)
+    .text(function(d) { return d.title });
+
 }
 
-function formatData(rawData) {
-  return _.pluck(_.pluck(rawData.data.children,"data"),"title")
+function formatSingleSubreddit(rawData) {
+  return _.pluck(rawData.data.children,"data")
 }
 
 main();
